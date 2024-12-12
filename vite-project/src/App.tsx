8bc9396d96db1d80ebdfb9e2c9ff8400a07eba80
Jaroslaw <Jarositek123456@gmail.com>
2024-12-12 19:34:22 +0100

    clipboard and small changes

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,26 +4,50 @@ import { HiCodeBracketSquare } from "react-icons/hi2";
 import { IoLogoCss3 } from "react-icons/io";
 import { RiJavascriptFill } from "react-icons/ri";
 import { MdCloseFullscreen } from "react-icons/md";
+import { IoIosCopy } from "react-icons/io";
 import IframeContent from "./Components/IframeContent";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Styl dla toasts
 interface StorageData {
   HTML: string;
   CSS: string;
   JAVASCRIPT: string;
 }
-const CodeEditor = ({ title, icon, language, value, onChange }: any) => (
-  <div className="h-[17rem] w-[31%] flex flex-col bg-[#080404] border-[#2f2f2f] border-[1px] relative">
+const notify = (language: string) => {
+  toast.success(`Udało się skopiować ${language.toUpperCase()}!`);
+};
+
+const CodeEditor = ({ id, title, icon, language, value, onChange }: any) => (
+  <div
+    className="h-[17rem] flex flex-col flex-1 basis-0 bg-[#080404] border-[#2f2f2f] border-[1px] overflow-auto"
+    id={id}
+  >
     <div className="flex h-[12%] justify-between items-center">
       <div className="bg-[#1e1e1e] w-[6.5rem] h-[100%] border-[#2f2f2f] border-t-2">
-        <p className="flex items-center justify-center h-[100%] w-[100%] text-xl font-bold text-[#aaaebc] gap-2">
+        <p className="flex items-center justify-center h-[100%] w-[100%] text-xl font-bold text-[#aaaebc] gap-2 select-none ">
           {icon}
           {title}
         </p>
       </div>
-      {/* moze dorobic diva aby lepiej wygladalo */}
-      <MdCloseFullscreen className="fill-white mr-4 text-lg cursor-pointer h-[100%]"></MdCloseFullscreen>
-    </div>
+      <div className="flex gap-4 items-center">
+        <IoIosCopy
+          className="fill-white cursor-pointer hover:scale-110 duration-200"
+          onClick={() => {
+            navigator.clipboard.writeText(value);
+            notify(language);
+          }}
+        ></IoIosCopy>
+        <MdCloseFullscreen
+          className="fill-white mr-4 text-lg cursor-pointer h-[100%] hover:scale-110 duration-200"
+          onClick={() => {
+            const box = document.getElementById(id) as HTMLDivElement;
+            box.style.flexGrow = box.style.flexGrow === "4" ? "1" : "4";
+          }}
+        ></MdCloseFullscreen>
+      </div>
+    </div>{" "}
     <Editor
-      width="100%"
+      className="overflow-hidden"
       height="88%"
       defaultLanguage={language}
       theme="vs-dark"
@@ -52,7 +76,6 @@ const App = () => {
   useEffect(() => {
     if (item !== null) {
       let ParsedItem: StorageData = JSON.parse(item);
-      console.log(ParsedItem);
       setHTML(ParsedItem.HTML);
       setCSS(ParsedItem.CSS);
       setJAVASCRIPT(ParsedItem.JAVASCRIPT);
@@ -61,8 +84,22 @@ const App = () => {
 
   return (
     <>
-      <div className="flex justify-center gap-4 w-[100vw] h-[20rem] bg-gradient-to-bl from-[#080404] via-[#1e1e1e] to-[#121212] pt-6">
+      {" "}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <div className="flex gap-4 px-4 w-[100vw] h-[20rem] bg-gradient-to-bl from-[#080404] via-[#1e1e1e] to-[#121212] pt-6 ">
         <CodeEditor
+          id="Editor0"
           title="HTML"
           icon={<HiCodeBracketSquare className="fill-red-500" />}
           language="html"
@@ -70,6 +107,7 @@ const App = () => {
           onChange={(value: string) => setHTML(value || "")}
         />
         <CodeEditor
+          id="Editor1"
           title="CSS"
           icon={<IoLogoCss3 className="fill-blue-500" />}
           language="css"
@@ -77,6 +115,7 @@ const App = () => {
           onChange={(value: string) => setCSS(value || "")}
         />
         <CodeEditor
+          id="Editor2"
           title="JS"
           icon={<RiJavascriptFill className="fill-yellow-400" />}
           language="javascript"
@@ -92,5 +131,4 @@ const App = () => {
 };
 
 export default App;
-// zrobic localstrogae jutro
-// responsywnosc oraz minimalizowanie okienek
+//dodac responsywnosc oraz resizwolnosc glownych divow
